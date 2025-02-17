27fc01d51cf070dddec8ed6529ba2b7685a27cd2
Michael Lee LaPointe <79478829+MichaelLaPointe@users.noreply.github.com>
2025-02-16 19:21:01 -0600

    Update AnalyzeSampleScript.ps1

--- Coding with AI - Part One - Using AI to help Analyze Scripts/AnalyzeSampleScript.ps1
+++ Coding with AI - Part One - Using AI to help Analyze Scripts/AnalyzeSampleScript.ps1
@@ -1,3 +1,3 @@
-Script to analyze
+# Script to analyze in ChatGPT, Claude AI, and DeepSeek R1
 
 $r=@();ps|%{if($_.CPU-gt 100){$r+=[PSCustomObject]@{Name=$_.Name;CPU=$_.CPU;Memory=($_.WorkingSet/1MB).ToString("N2")+" MB"}}};$r|Sort CPU -Descending|epcsv -Path "H.csv" -NoTypeInformation
